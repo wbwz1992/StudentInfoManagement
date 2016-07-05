5412226643e372fdce9f0dc2c1634cd577f05da4
robin <wbwz1992@gmail.com>
2016-07-05 19:02:54 +0800

    1

--- StudentInfoManagement/StudentForm.cs
+++ StudentInfoManagement/StudentForm.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using StudentInfoMangement;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -37,9 +39,17 @@ namespace StudentInfoManagement
 
         private void majorBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string[] majors = {"信息与计算科学", "数学与应用数学", "应用物理" };
-            string[] classes = { "1241", "1242", "1341", "1342" };
-            string major = majors[majorBox.SelectedIndex];
+            string[] majors = {"信息与计算科学", "数学与应用数学", "应用物理" };           
+            string connectionString = "Data Source=DESKTOP-NOTL3M1;Initial Catalog=StudentManagementDB;Integrated Security=True";
+            string sql = "SELECT class_num FROM Class WHERE class_major= @major";
+            DataSet dateset = SqlHelper.ExecuteDataset(connectionString, CommandType.Text, sql, new SqlParameter[] {
+            new SqlParameter("@major", majors[majorBox.SelectedIndex])});
+            int n = dateset.Tables[0].Columns.Count;
+            string[] classes = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                classes[i] = dateset.Tables[0].Rows[0][i].ToString().Trim();
+            }
             classBox.Items.Clear();
             classBox.Items.AddRange(classes);
 
